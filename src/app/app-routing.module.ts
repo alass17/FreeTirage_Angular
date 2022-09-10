6c0695724589180775f259eb909d687f69fef426
amalle <alassanemalle733@gmail.com>
2022-09-10 19:49:38 +0000

    Essai

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailsTirageComponent } from './details-tirage/details-tirage.component';
+import { TirageComponent } from './tirage/tirage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'tirage' ,component:TirageComponent},
+  {path:'Détail' ,component:DetailsTirageComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
