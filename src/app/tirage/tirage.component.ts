92facf2c84e6d90cffe8413cc94c219ca87e812f
amalle <alassanemalle733@gmail.com>
2022-09-16 15:24:53 +0000

    modification

--- src/app/tirage/tirage.component.ts
+++ src/app/tirage/tirage.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AcceuilListService } from '../acceuil-list.service';
 import { Import } from '../import';
-import { ListePostulantComponent } from '../liste-postulant/liste-postulant.component';
+import { Listpost } from '../listpost';
 import { Tirageclasse } from '../tirageclasse';
 import { TirageserviceService } from '../tirageservice.service';
+
 
 @Component({
   selector: 'app-tirage',
@@ -14,17 +16,25 @@ export class TirageComponent implements OnInit {
   tirage: Tirageclasse = new Tirageclasse();
   liste!:Import;
   nombree:any
+  // listeposts!:Listpost[];
+  // listepost: Listpost=new Listpost ();
 
 
-  constructor(private tirageService:TirageserviceService) { }
+  constructor(private tirageservice:TirageserviceService, private accueilService:AcceuilListService) { }
 
   ngOnInit(): void {
+    // this.getListe()
   }
   SauverTirage(){
-    this.tirageService.CreateTirage(this.tirage,this.libelle,this.nombree).subscribe(data =>{
+    this.tirageservice.CreateTirage(this.tirage,this.libelle,this.nombree).subscribe(data =>{
       console.log(data);
     })
   }
+  // private getListe(){
+  //   this.accueilService.getAcceuilList().subscribe(data =>{
+  //     this.libelle=data;
+  //   })
+
   OnSubmit(){
     console.log(this.tirage);
     this.SauverTirage();
